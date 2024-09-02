748a9fcf4b7a5f43aa0f3ed9ca183db7f8f93f37
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-09-02 13:45:46 +0300

    add Contact Scopes link to apps that have it enabled

--- quickstep/src/com/android/quickstep/TaskOverlayFactory.java
+++ quickstep/src/com/android/quickstep/TaskOverlayFactory.java
@@ -118,6 +118,7 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
             TaskShortcutFactory.SCREENSHOT,
             TaskShortcutFactory.MODAL,
             TaskShortcutFactory.STORAGE_SCOPES,
+            TaskShortcutFactory.CONTACT_SCOPES,
     };
 
     /**
